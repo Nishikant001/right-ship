10b9b5e828e1b6fa3745807622ce40923bbdd8b2
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-08-11 14:53:37 +0530

    In Progress

--- src/job_seeker/login/JobLogin.jsx
+++ src/job_seeker/login/JobLogin.jsx
@@ -5,7 +5,7 @@ import { setContactInfo } from '../../features/contactSlice';
 import logo from "../../images/logo.png";
 import { useNavigate } from 'react-router-dom';
 
-const JobLogin = () => {
+const Login = () => {
 
   const [phoneNumber, setPhoneNumber] = useState('');
   const dispatch = useDispatch();
@@ -20,30 +20,30 @@ const JobLogin = () => {
   };
 
   return (
-    <section className="flex flex-col items-center my-20 h-screen">
-      <div className="text-2xl font-bold mb-4">
-        <img src={logo} alt="Logo" height={70} width={70} />
+    <section className="flex flex-col items-center py-20 h-screen bg-gray-100">
+      <div className="mb-4">
+        <img src={logo} alt="Logo" className="h-24 w-20" />
       </div>
-      <div className="bg-white p-6 mt-3 rounded-lg shadow-2xl border w-full max-w-md">
-        <h2 className="text-center text-xl font-bold mb-4">Login to Rightship</h2>
+      <div className="bg-white p-10 mt-3 rounded-lg shadow-lg border w-full max-w-md">
+        <h2 className="text-center text-2xl font-semibold mb-6">Log in to Rightship</h2>
         <input
           type="text"
           placeholder="Enter the phone number"
-          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded"
+          className="w-full px-4 py-4 mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
         <button
           onClick={handleSendOtp}
-          className="w-full bg-indigo-900 text-white px-4 py-2 rounded"
+          className={`w-full py-4 rounded-md text-white font-medium ${otpStatus === 'loading' ? 'bg-indigo-700' : 'bg-indigo-900 hover:bg-indigo-700'} transition duration-300`}
           disabled={otpStatus === 'loading'}
         >
           {otpStatus === 'loading' ? 'Sending...' : 'Send OTP'}
         </button>
-        {otpStatus === 'failed' && <p className="text-red-600 mt-4">{otpError}</p>}
+        {otpStatus === 'failed' && <p className="text-red-600 mt-4 text-center">{otpError}</p>}
       </div>
     </section>
   );
 };
 
-export default JobLogin;
+export default Login;
