84a2846ab8f267ce6bee8268dc68f0c0876a6b37
Nishikant <nishikantsahoo008@gmail.com>
2024-08-18 11:29:50 +0530

    nishi

--- src/component/navbar/companyNavbar.js
+++ src/component/navbar/companyNavbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CgProfile } from "react-icons/cg";
+import Logo from '../../images/logo.png'
 
 const CompanyNavbar = () => {
   return (
 <nav className=" p-4">
   <div className="container mx-auto flex justify-between items-center">
     <div className="flex space-x-4 flex-row ml-auto items-center -mt-2">
+    <Link to="/" className="flex ">
+              <img src={Logo} alt="Logo" height={40} width={40} />
+              <span className="font-bold text-gray-800 ml-4">RIGHTSHIP</span>
+    </Link>
       
       <Link
         to="/dashboard"
