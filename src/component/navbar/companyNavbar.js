aabd37cc70279ae0e2dc11581d206670595738f5
Nishikant <nishikantsahoo008@gmail.com>
2024-08-18 11:34:48 +0530

    logo chnages

--- src/component/navbar/companyNavbar.js
+++ src/component/navbar/companyNavbar.js
@@ -5,14 +5,9 @@ import Logo from '../../images/logo.png'
 
 const CompanyNavbar = () => {
   return (
-<nav className=" p-4">
-  <div className="container mx-auto flex justify-between items-center">
-    <div className="flex space-x-4 flex-row ml-auto items-center -mt-2">
-    <Link to="/" className="flex ">
-              <img src={Logo} alt="Logo" height={40} width={40} />
-              <span className="font-bold text-gray-800 ml-4">RIGHTSHIP</span>
-    </Link>
-      
+<nav className="p-4 relative">
+  <div className="container mx-auto flex justify-center items-center">
+    <div className="flex space-x-4 flex-row absolute top-2 right-0 items-center">
       <Link
         to="/dashboard"
         className="text-black px-3 py-2 rounded flex items-center space-x-2 transition duration-200"
@@ -28,8 +23,18 @@ const CompanyNavbar = () => {
         <CgProfile size={24} /> {/* Increase the size as needed */}
       </Link>
     </div>
+
+    {/* Right top logo */}
+    <Link
+      to="/"
+      className="absolute top-0 left-0 mt-2 mr-4 flex items-center"
+    >
+      <img src={Logo} alt="Logo" height={40} width={40} />
+      <span className="font-bold text-gray-800 ml-2">RIGHTSHIP</span>
+    </Link>
   </div>
 </nav>
+
   
   );
 };
